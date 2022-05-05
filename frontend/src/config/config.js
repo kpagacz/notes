3d84a24fd9971a5d089b8c6f65621022be9fe6b3
Konrad Pagacz <konrad.pagacz@gmail.com>
2022-05-05 13:01:35 +0200

    Update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,4 @@
-const baseApiUrl = "" // Here goes the base Api URL of the backend service
+const baseApiUrl = "https://3c3cwwoylb.execute-api.eu-central-1.amazonaws.com/" // Here goes the base Api URL of the backend service
 
 const config = {
   prod: {
